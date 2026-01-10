a3bcf6b7c9b775b178242f51c0c7adf0f3bb5258
Kostya Malinovskiy <kostya.malinovskiy@gmail.com>
2026-01-10 09:34:33 +0200

    Decide what to do in request parse depending on state. Extend buffer when filled for around a half. Shift read bytes to the beginning of buffer if some bytes where parsed

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,92 +4,121 @@ import(
 	"io"
 	"fmt"
 	"strings"
+	"bytes"
 )
 
-type Request struct{
-	RequestLine RequestLine
-	state int
-}
+var SEPARATOR = []byte("\r\n")
+
+type parserState int
+const (
+	StateInit parserState = 0
+	StateDone parserState = 1
+)
 
 type RequestLine struct {
 	Method string
 	RequestTarget string
 	HttpVersion string
-	state int
 }
 
+type Request struct{
+	RequestLine RequestLine
+	state parserState
+}
+
+func newRequest() Request {
+	return Request{
+		state: StateInit,
+	}
+}
+
+const buffSize = 8
 func RequestFromReader(reader io.Reader) (*Request, error){
 	bytesReadCount := 0
 	bytesParsedCount := 0
+	readBuf := make([]byte, buffSize, buffSize)
 
-	req := Request{ state: 0 }
-	readBuf := make([]byte, 8)
-	accBuf := make([]byte, 0) 
-	for {
-		n, err := reader.Read(readBuf)
-		fmt.Printf("Read: %q, n: %v\n", readBuf[:n], n)
-		bytesReadCount += n
+	fmt.Println(">>>>")
+	req := newRequest()
+	for !req.isDone() {
+		n, err := reader.Read(readBuf[bytesReadCount:])
+		fmt.Printf("Read: %q, n: %v\n", readBuf, n)
 		if err != nil && err == io.EOF {
 			break
 		}
 		if err != nil {
 			return &req, err 
 		}
+		bytesReadCount += n
 
-		accBuf = append(accBuf, readBuf[:n]...)
-		n, err = req.parse(accBuf)
-		bytesParsedCount += n
+		if bytesReadCount >= len(readBuf) / 2 {
+
+			fmt.Println("extending buffer")
+			newSize := bytesReadCount*2
+			newBuf := make([]byte, newSize, newSize)
+			copy(newBuf, readBuf)
+			readBuf = newBuf
+		}
+
+		fmt.Printf("Parse: %q, bytesReadCount: %v\n", readBuf[:bytesReadCount], bytesReadCount)
+		bytesParsedCount, err = req.parse(readBuf[:bytesReadCount])
+
 
 		if err != nil {
 			return &req, err 
 		}
 
-		if req.isDone() {
-			break
+		if bytesParsedCount > 0 {
+			fmt.Errorf("Removing parsed part")
+			copy(readBuf, readBuf[bytesParsedCount:])
+			bytesReadCount -= bytesParsedCount
 		}
+		fmt.Println("----")
 	}
+	fmt.Println("<<<<")
 	return &req, nil
 }
 
 
 func (r *Request) parse(data []byte) (int, error){
-	reqLine, n, err := parseRequestLine(data)
+	fmt.Printf("received to parse: %q\n", data) 
+	switch r.state {
+	case StateInit:
+		reqLine, n, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
 
-	if err != nil {
-		return 0, err
+		if n > 0 {
+			r.RequestLine = reqLine
+			r.state = StateDone
+		}
+		return n, nil
+	case StateDone:
+		return 0, nil
+	default:
+		return 0, nil
 	}
-
-	if n > 0 {
-		r.RequestLine = reqLine
-		r.state = 1
-	}
-
-	return n, nil
 }
 func (r *Request) isDone() bool{
-	return r.state == 1
+	return r.state == StateDone
 }
 
 func parseRequestLine(data []byte) (RequestLine, int, error) {
 	var reqLine RequestLine
-	bytesReadCount := 0
-	lineEnded := false
-	rawLine := ""
-	for i, b := range(data) {
-		bytesReadCount += 1
-		rawLine += string(b)
-		if i > 0 && data[i-1] == '\r' && data[i] == '\n' {
-			lineEnded = true
-			break
-		}
-	}
+	
+	sepIndex := bytes.Index(data, SEPARATOR)
 
-	if !lineEnded {
+	if sepIndex == -1 {
 		return reqLine, 0, nil
 	}
 
-	parsed := strings.Trim(rawLine, "\r\n")
-	parts := strings.Split(parsed, " ")
+
+	rawLine := data[:sepIndex]
+	//leftOver := data[sepindex + len(SEPARATOR):]
+	bytesReadCount := len(rawLine)
+
+	parts := strings.Split(string(rawLine), " ")
 
 	if len(parts) != 3 {
 		return reqLine, bytesReadCount, fmt.Errorf("Error parsing request line")
@@ -100,5 +129,6 @@ func parseRequestLine(data []byte) (RequestLine, int, error) {
 	reqLine.Method = parts[0]
 	reqLine.RequestTarget = parts[1]
 	reqLine.HttpVersion = split_version[1]
+
 	return reqLine, bytesReadCount, nil
 }
