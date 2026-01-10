37469b37f87d89445a75cd9cf1c87e0fe6269d02
Kostya Malinovskiy <kostya.malinovskiy@gmail.com>
2026-01-10 22:45:44 +0200

    Implement suggested solution

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -5,9 +5,7 @@ import(
 	"fmt"
 )
 var SEPARATOR = []byte("\r\n")
-var HEADERS_END = []byte("\r\n\r\n")
 var NAME_VALUE_SEPARATOR = []byte(":")
-//var WS = []byte(" ")
 var WS = " "
 
 var HEADER_PARSE_ERROR = fmt.Errorf("Error parsing header")
@@ -18,24 +16,25 @@ func NewHeaders() Headers {
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	headersEndIndex := bytes.Index(data, HEADERS_END)
-	if headersEndIndex == -1 {
+	headersSepIndex := bytes.Index(data, SEPARATOR)
+	if headersSepIndex == -1 {
 		return 0, false, nil
+	}else if headersSepIndex == 0{
+		return 0, true, nil
 	}
 
-	headerLines := bytes.Split(data[:headersEndIndex], SEPARATOR)
+	headerLine := data[:headersSepIndex]
 	
-	for _, headerLine := range(headerLines) {
-		sepIndex := bytes.Index(headerLine, []byte(":"))
+	sepIndex := bytes.Index(headerLine, NAME_VALUE_SEPARATOR)
 
-		name := headerLine[:sepIndex]
-		val := headerLine[sepIndex+1:]
-		if bytes.HasSuffix(name, []byte(WS))  {
-			return 0, false, HEADER_PARSE_ERROR
-		}
+	name := headerLine[:sepIndex]
+	val := headerLine[sepIndex+1:]
 
-		h[string(bytes.Trim(name, WS))] = string(bytes.Trim(val, WS))
+	if bytes.HasSuffix(name, []byte(WS))  {
+		return 0, false, HEADER_PARSE_ERROR
 	}
+
+	h[string(bytes.Trim(name, WS))] = string(bytes.Trim(val, WS))
 	
-	return headersEndIndex + 2, true, nil
+	return headersSepIndex + 2, false, nil
 }
